903f4e21c9b2147c653dd303872a1e6422cbca5d
kiki-kanri <a470666@gmail.com>
2023-08-02 11:59:00 +0800

    Add install mariadb script.

---
+++ scripts/install_mariadb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install mariadb 11.0.2
+
+cd /tmp/
+sudo curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup &&
+    sudo bash mariadb_repo_setup --mariadb-server-version=11.0.2 &&
+	sudo rm /etc/apt/sources.list.d/mariadb.list.* &&
+	sudo apt-get install mariadb-server &&
+	sudo systemctl enable mariadb &&
+	sudo systemctl start mariadb &&
+	sudo rm mariadb_repo_setup
