63d2d9439c1469c314f34bdfe5b82bbf3509b8d8
kiki-kanri <83924639+kiki-kanri@users.noreply.github.com>
2023-08-02 13:19:02 +0800

    Fixed install mariadb error.

--- scripts/install_mariadb.sh
+++ scripts/install_mariadb.sh
@@ -5,7 +5,7 @@
 cd /tmp/
 sudo curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup &&
     sudo bash mariadb_repo_setup --mariadb-server-version=11.0.2 &&
-	sudo rm /etc/apt/sources.list.d/mariadb.list.* &&
+	sudo rm -rf /etc/apt/sources.list.d/mariadb.list.* &&
 	sudo apt-get install mariadb-server &&
 	sudo systemctl enable mariadb &&
 	sudo systemctl start mariadb &&
