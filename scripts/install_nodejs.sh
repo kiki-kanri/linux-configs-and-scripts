5beae698de114638dcddab01ef23affb86366942
kiki-kanri <a470666@gmail.com>
2023-09-30 11:46:00 +0800

    feat: setup package managers after installed nodejs

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -6,5 +6,9 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash - &&
 	sudo apt-get update &&
 	sudo apt-get install nodejs &&
 	sudo npm i npm@latest -g &&
+	sudo npm upgrade -g &&
 	sudo corepack enable &&
-	sudo corepack prepare pnpm@latest --activate
+	sudo corepack install --global pnpm@latest &&
+	sudo corepack install --global yarn@stable &&
+	corepack install --global pnpm@latest &&
+	corepack install --global yarn@stable
