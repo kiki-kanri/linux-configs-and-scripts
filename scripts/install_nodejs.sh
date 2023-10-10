0cd40788635a8b9433b32f04406d43e181b0d20a
kiki-kanri <a470666@gmail.com>
2023-10-10 12:25:55 +0800

    chore(script): edit install nodejs and portainer

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -2,12 +2,10 @@
 
 # Install nodejs 20
 
-NODE_MAJOR=20
-
 sudo apt-get update &&
 	sudo apt-get install -y ca-certificates curl gnupg &&
 	curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg &&
-	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list &&
+	echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list &&
 	sudo apt-get update &&
 	sudo apt-get install nodejs &&
 	sudo npm i npm@latest -g &&
--- scripts/upgrade_portainer.sh
+++ scripts/upgrade_portainer.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-docker pull portainer/portainer-ce:latest &&
-	docker kill portainer &&
-	docker rm portainer &&
-	docker run -d -p 127.0.0.1:9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+cointainer_name="portainer"
+
+docker pull portainer/portainer-ce:latest || exit 1
+[ "$(docker ps | grep $cointainer_name)" ] && docker kill $cointainer_name
+[ "$(docker ps -a | grep $cointainer_name)" ] && docker rm $cointainer_name
+docker run \
+	-d \
+	-p 127.0.0.1:9000:9000 \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v portainer_data:/data \
+	--name $cointainer_name \
+	--restart=always \
+	portainer/portainer-ce:latest
