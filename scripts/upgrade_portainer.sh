125a2d1088cdba655de81b2f147f02cb04e2e9ed
kiki-kanri <a470666@gmail.com>
2023-10-10 19:40:59 +0800

    fix(script): correct spelling of "container"

--- scripts/upgrade_portainer.sh
+++ scripts/upgrade_portainer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cointainer_name="portainer"
+container_name="portainer"
 
 docker pull portainer/portainer-ce:latest || exit 1
-[ "$(docker ps | grep $cointainer_name)" ] && docker kill $cointainer_name
-[ "$(docker ps -a | grep $cointainer_name)" ] && docker rm $cointainer_name
+[ "$(docker ps | grep $container_name)" ] && docker kill $container_name
+[ "$(docker ps -a | grep $container_name)" ] && docker rm $container_name
 docker run \
 	-d \
 	-p 127.0.0.1:9000:9000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v portainer_data:/data \
-	--name $cointainer_name \
+	--name $container_name \
 	--restart=always \
 	portainer/portainer-ce:latest
