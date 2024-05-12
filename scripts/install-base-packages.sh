bfaae538c31537cd602f6a040fac561a041db102
kiki-kanri <a470666@gmail.com>
2024-05-12 13:29:21 +0800

    feat: add acl package to install-base-packages script

--- scripts/install-base-packages.sh
+++ scripts/install-base-packages.sh
@@ -3,6 +3,6 @@
 # Install base packages
 
 sudo apt-get update &&
-	sudo apt-get install curl gcc htop make net-tools perl screen tar tmux unzip vim wget &&
+	sudo apt-get install acl curl gcc htop make net-tools perl screen tar tmux unzip vim wget &&
 	sudo apt-get autoremove --purge &&
 	sudo apt-get autoclean
