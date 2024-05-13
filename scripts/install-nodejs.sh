3b920d408bcf3a482c7c9f5bb330cad009534b29
kiki-kanri <a470666@gmail.com>
2024-05-14 01:01:30 +0800

    fix: correct command sequence in post-installation of nodejs

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -6,6 +6,6 @@ sudo apt-get update &&
 	curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash - &&
 	sudo apt-get update &&
 	sudo apt-get install -qy nodejs &&
+	sudo npm upgrade -g &&
 	sudo corepack disable pnpm &&
-	sudo npm i npm@latest pnpm@latest -g &&
-	sudo npm upgrade -g
+	sudo npm i npm@latest pnpm@latest -g
