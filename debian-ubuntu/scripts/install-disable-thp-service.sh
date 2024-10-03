b9f55daf4a3683225d32cc7a04d6322c19b3b801
kiki-kanri <a470666@gmail.com>
2024-10-03 23:40:54 +0800

    fix: correct errors in `install-disable-thp-service.sh`

--- debian-ubuntu/scripts/install-disable-thp-service.sh
+++ debian-ubuntu/scripts/install-disable-thp-service.sh
@@ -4,7 +4,7 @@ ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
 cd $ROOT_DIR
 . ./scripts/common.sh
 
-sudo cp ./etc/services/disable-thp.service /etc/systemd/system/ &&
+sudo cp ./etc/systemd/system/disable-thp.service /etc/systemd/system/ &&
 	sudo systemctl daemon-reload &&
 	sudo systemctl enable disable-thp.service &&
 	sudo systemctl start disable-thp.service
