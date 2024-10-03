35b7ad0bfd6472ef158e73d377a4e3432701f57f
kiki-kanri <a470666@gmail.com>
2024-10-04 00:15:16 +0800

    fix: resolve issues with `build-and-install-nginx.sh` on Ubuntu

--- debian-ubuntu/scripts/build-and-install-nginx.sh
+++ debian-ubuntu/scripts/build-and-install-nginx.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-NGINX_VERSION='1.26.2'
 ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
-cd $ROOT_DIR
+cd "$ROOT_DIR"
 . ./scripts/common.sh
+
+CC_OPT_FLAG='-ffat-lto-objects -flto=4 -fPIC -fstack-protector-strong -g -march=native -O3 -pthread -Werror=format-security -Wformat'
+DEVELOP_PACKAGES='libbrotli-dev libgeoip-dev libpcre3-dev libperl-dev libssl-dev libzstd-dev zlib1g-dev'
+NGINX_VERSION='1.26.2'
+RUNTIME_PACKAGES='geoip-bin geoip-database libbrotli1 libgeoip1 libpcre3 libperl5.* libssl3 libzstd1 zlib1g'
+[ "$os_type" = 'debian' ] && CC_OPT_FLAG+=' -Wp,-D_FORTIFY_SOURCE=2'
 
 # Clone brotli module
 cd /tmp &&
@@ -20,7 +25,7 @@ cd /tmp &&
 	git submodule update --init --recursive &&
 
 	# Build nginx
-	sudo apt-get install -y libbrotli-dev libgeoip-dev libpcre3-dev libperl-dev libssl-dev libzstd-dev &&
+	sudo apt-get install -y $DEVELOP_PACKAGES &&
 	cd /tmp &&
 	rm -rf nginx-$NGINX_VERSION nginx-$NGINX_VERSION.tar.gz* &&
 	wget http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz &&
@@ -44,7 +49,7 @@ cd /tmp &&
 		--prefix=/etc/nginx \
 		--sbin-path=/usr/sbin/nginx \
 		--user=nginx \
-		--with-cc-opt='-ffat-lto-objects -flto=4 -fPIC -fstack-protector-strong -g -march=native -O3 -pthread -Werror=format-security -Wformat -Wp,-D_FORTIFY_SOURCE=2' \
+		--with-cc-opt="$CC_OPT_FLAG" \
 		--with-compat \
 		--with-file-aio \
 		--with-http_addition_module \
@@ -85,16 +90,17 @@ cd /tmp &&
 	make -j$(nproc) &&
 	sudo make install &&
 	sudo mkdir -p /var/cache/nginx /var/log/nginx &&
-	sudo apt-get remove -y --auto-remove --purge libbrotli-dev libgeoip-dev libpcre3-dev libperl-dev libssl-dev libzstd-dev &&
-	sudo apt-get install -y geoip-bin geoip-database libbrotli1 libgeoip1 libpcre3 libperl5.* libssl3 libzstd1 &&
+	sudo apt-get remove -y --auto-remove --purge $DEVELOP_PACKAGES &&
+	sudo apt-get install -y $RUNTIME_PACKAGES &&
 	cd /tmp &&
 	rm -rf nginx-$NGINX_VERSION ngx_brotli zstd-nginx-module &&
 
 	# Configure nginx
+	id -u nginx &>/dev/null || sudo useradd -r -s /sbin/nologin nginx &&
 	sudo mkdir -p /etc/nginx/certs &&
 	sudo openssl dhparam -dsaparam -out /etc/nginx/certs/dhparam.pem 4096 &&
 	sudo rm -rf /etc/nginx/nginx.conf &&
-	cd $ROOT_DIR &&
+	cd "$ROOT_DIR" &&
 	sudo cp -r ./etc/nginx /etc/ &&
 	sudo cp ./etc/systemd/system/nginx.service /etc/systemd/system/ &&
 	sudo systemctl daemon-reload &&
