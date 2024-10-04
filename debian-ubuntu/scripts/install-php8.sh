c797aff305d8619bd3e8d26814f4592eebd42b49
kiki-kanri <a470666@gmail.com>
2024-10-04 21:39:38 +0800

    feat: install additional PHP extensions

--- debian-ubuntu/scripts/install-php8.sh
+++ debian-ubuntu/scripts/install-php8.sh
@@ -7,6 +7,6 @@ cd "$ROOT_DIR"
 sudo apt-get update &&
 	sudo apt-get install apt-transport-https ca-certificates lsb-release software-properties-common &&
 	sudo add-apt-repository ppa:ondrej/php &&
-	sudo apt-get install php8.3 php8.3-{bcmath,cli,common,curl,fpm,gd,imap,mbstring,mysql,readline,redis,xml,zip} &&
+	sudo apt-get install php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,gmp,http,imagick,imap,mbstring,mongodb,mysql,readline,redis,swoole,xml,yaml,zip} &&
 	sudo systemctl enable php8.3-fpm &&
 	sudo systemctl start php8.3-fpm
