1fa15fb5ea3e0a7d25dbb7d9e628944c21a99dd4
kiki-kanri <a470666@gmail.com>
2024-10-04 10:33:01 +0800

    feat: add script to install 7zip

---
+++ debian-ubuntu/scripts/install-7zip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
+cd "$ROOT_DIR"
+. ./scripts/common.sh
+
+sudo mkdir -p /opt/7zip &&
+	cd /opt/7zip &&
+	sudo wget https://www.7-zip.org/a/7z2408-linux-x64.tar.xz &&
+	sudo tar -xvf 7z2408-linux-x64.tar.xz &&
+	sudo rm 7z2408-linux-x64.tar.xz &&
+	sudo ln -s /opt/7zip/7zz /usr/local/bin/7zz &&
+	sudo ln -s /opt/7zip/7zzs /usr/local/bin/7zzs
