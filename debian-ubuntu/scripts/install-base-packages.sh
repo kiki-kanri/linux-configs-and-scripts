11bcbc9c03e651d0bea529ac383db394fe0cd1d2
kiki-kanri <a470666@gmail.com>
2024-10-07 09:26:03 +0800

    chore: update base package installation list

--- debian-ubuntu/scripts/install-base-packages.sh
+++ debian-ubuntu/scripts/install-base-packages.sh
@@ -5,6 +5,6 @@ cd "$ROOT_DIR"
 . ./scripts/common.sh
 
 sudo apt-get update &&
-	sudo apt-get install -y acl colormake curl gcc htop iftop iotop lsof nmap net-tools perl screen tar tmux unzip vim wget &&
+	sudo apt-get install -y acl colormake cron curl g++ gcc htop iftop iotop iputils-ping lsof nmap net-tools perl screen tar tmux unzip vim wget &&
 	sudo apt-get autoremove -y --purge &&
 	sudo apt-get autoclean
