2bc58c34ff3041f18a3d80a43197be1b8b230efa
kiki-kanri <a470666@gmail.com>
2024-10-30 11:04:46 +0800

    chore: add -fp option to some commands

--- debian-ubuntu/scripts/disable-ipv6.sh
+++ debian-ubuntu/scripts/disable-ipv6.sh
@@ -4,5 +4,5 @@ ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
 cd "$ROOT_DIR"
 . ./scripts/common.sh
 
-cp ./etc/sysctl.d/disable-ipv6.conf /etc/sysctl.d/ &&
+sudo cp -fp ./etc/sysctl.d/disable-ipv6.conf /etc/sysctl.d/ &&
 	sudo sysctl --system
--- debian-ubuntu/scripts/install-cat-motd.sh
+++ debian-ubuntu/scripts/install-cat-motd.sh
@@ -4,5 +4,5 @@ ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
 cd "$ROOT_DIR"
 . ./scripts/common.sh
 
-sudo cp ./etc/update-motd.d/9999-cat /etc/update-motd.d/ &&
+sudo cp -fp ./etc/update-motd.d/9999-cat /etc/update-motd.d/ &&
 	sudo chmod +x /etc/update-motd.d/9999-cat
