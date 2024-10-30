838ee98521089429aba071b43bdcd0a805b0385f
kiki-kanri <a470666@gmail.com>
2024-10-30 11:00:34 +0800

    feat: update Docker-related files and add configurations for MongoDB 8 THP support

--- data/services/docker-build-and-run.sh
+++ data/services/docker-build-and-run.sh
@@ -6,4 +6,5 @@ if [ "$1" = '-p' ]; then
 	docker compose pull
 fi
 
-docker compose build && docker compose up -d --remove-orphans
+gcc -flto=4 -march=native -fPIC -pthread -O3 -shared -o ./libforce_enable_thp.so ./force_enable_thp.c -ldl -lpthread &&
+	docker compose build && docker compose up -d --remove-orphans
