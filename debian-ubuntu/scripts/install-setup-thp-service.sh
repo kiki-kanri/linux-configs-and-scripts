345fa31e0aafa83f58706d5ec3cf3f8feadbd7cf
kiki-kanri <a470666@gmail.com>
2024-10-30 11:02:39 +0800

    refactor: rename `install-disable-thp-service.sh` to `install-setup-thp-service.sh` and update related files

---
+++ debian-ubuntu/scripts/install-setup-thp-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
+cd "$ROOT_DIR"
+. ./scripts/common.sh
+
+sudo cp -fp ./etc/systemd/system/setup-thp.service /etc/systemd/system/ &&
+	sudo systemctl daemon-reload &&
+	sudo systemctl enable setup-thp.service &&
+	sudo systemctl start setup-thp.service
