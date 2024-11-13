9e0bc7c29bbf53fdb5d9a847469d823ab3d70549
kiki-kanri <a470666@gmail.com>
2024-11-13 17:09:23 +0800

    chore: modify parameters used when building force_enable_thp.c

--- data/services/docker-build-and-run.sh
+++ data/services/docker-build-and-run.sh
@@ -6,5 +6,5 @@ if [ "$1" = '-p' ]; then
 	docker compose pull
 fi
 
-gcc -march=native -fPIC -O3 -shared -Werror=format-security -Wformat -o ./libforce_enable_thp.so ./force_enable_thp.c -ldl &&
+gcc -fPIC -fstack-protector-strong -march=native -O3 -shared -Werror=format-security -Wformat -o ./libforce_enable_thp.so ./force_enable_thp.c -ldl -Wl,--as-needed -Wl,-O2 -Wl,-z,now -Wl,-z,relro &&
 	docker compose build && docker compose up -d --remove-orphans
