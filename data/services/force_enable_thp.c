f7f8d54c5d73f4ef73f5ad76c5bf1dda12edec6e
kiki-kanri <a470666@gmail.com>
2024-11-16 12:50:02 +0800

    style: add `.clang-format` file and format codes

--- data/services/force_enable_thp.c
+++ data/services/force_enable_thp.c
@@ -3,15 +3,10 @@
 #include <stdio.h>
 #include <sys/mman.h>
 
-void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
-{
-	static void *(*real_mmap)(void *, size_t, int, int, int, off_t) = NULL;
-	if (!real_mmap)
-	{
-		real_mmap = dlsym(RTLD_NEXT, "mmap");
-	}
-
-	void *result = real_mmap(addr, length, prot, flags, fd, offset);
-	madvise(result, length, MADV_HUGEPAGE);
-	return result;
+void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
+    static void *(*real_mmap)(void *, size_t, int, int, int, off_t) = NULL;
+    if (!real_mmap) real_mmap = dlsym(RTLD_NEXT, "mmap");
+    void *result = real_mmap(addr, length, prot, flags, fd, offset);
+    madvise(result, length, MADV_HUGEPAGE);
+    return result;
 }
