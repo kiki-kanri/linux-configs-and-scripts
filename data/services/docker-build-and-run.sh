a55b02c4abb88328413ffa3d93ee3a5116ef8e71
kiki-kanri <a470666@gmail.com>
2024-11-22 00:11:33 +0800

    style: format files

--- data/services/docker-build-and-run.sh
+++ data/services/docker-build-and-run.sh
@@ -6,5 +6,19 @@ if [ "$1" = '-p' ]; then
     docker compose pull
 fi
 
-gcc -fPIC -fstack-protector-strong -march=native -O3 -shared -Werror=format-security -Wformat -o ./libforce_enable_thp.so ./force_enable_thp.c -ldl -Wl,--as-needed -Wl,-O2 -Wl,-z,now -Wl,-z,relro &&
+gcc \
+    -fPIC \
+    -fstack-protector-strong \
+    -march=native \
+    -O3 \
+    -shared \
+    -Werror=format-security \
+    -Wformat \
+    -o ./libforce_enable_thp.so \
+    ./force_enable_thp.c \
+    -ldl \
+    -Wl,--as-needed \
+    -Wl,-O2 \
+    -Wl,-z,now \
+    -Wl,-z,relro &&
     docker compose build && docker compose up -d --remove-orphans
--- debian-ubuntu/etc/nginx/domains/ssl-example.conf.d
+++ debian-ubuntu/etc/nginx/domains/ssl-example.conf.d
@@ -1,15 +1,15 @@
 server {
-	# Include configs
-	include public/headers/default.conf;
-	include public/ssls/example.conf;
+    # Include configs
+    include public/headers/default.conf;
+    include public/ssls/example.conf;
 
-	# Server name
-	server_name server_name;
+    # Server name
+    server_name server_name;
 
-	# Locations
-	location / {
-		include public/proxies/websocket.conf;
-		# proxy_set_header Origin "$scheme://$host";
-		proxy_pass http://127.0.0.1:3000;
-	}
+    # Locations
+    location / {
+        include public/proxies/websocket.conf;
+        # proxy_set_header Origin "$scheme://$host";
+        proxy_pass http://127.0.0.1:3000;
+    }
 }
