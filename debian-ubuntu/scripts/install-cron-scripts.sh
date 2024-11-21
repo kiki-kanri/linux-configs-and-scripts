6801a1287a6d77c45bf0a39ea05bdb7ddd811ca7
kiki-kanri <a470666@gmail.com>
2024-11-22 00:23:27 +0800

    feat: add script to install cron scripts

---
+++ debian-ubuntu/scripts/install-cron-scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ROOT_DIR="$(realpath "$(dirname "$(readlink -f "$0")")"/../)"
+cd "$ROOT_DIR"
+. ./scripts/common.sh
+
+for file in ./etc/cron.daily/*; do
+    filename=$(basename "$file")
+    read -p "Do you want to install $filename? (y/n) [y]: " choice
+    choice=${choice:-y}
+    case "$choice" in
+    [Yy]*)
+        cp -f "$file" /etc/cron.daily/ &&
+            chmod 755 "/etc/cron.daily/$filename" &&
+            chown root:root "/etc/cron.daily/$filename" &&
+            echo "$filename installed."
+        ;;
+
+    [Nn]*)
+        echo "$filename not installed."
+        ;;
+
+    *)
+        echo "Invalid choice. $filename not installed."
+        ;;
+    esac
+done
