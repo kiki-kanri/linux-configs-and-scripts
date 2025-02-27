c73e08d84aa09216a7d52a689d95d5a0280fe7c5
kiki-kanri <a470666@gmail.com>
2025-02-27 18:15:29 +0800

    refactor: modify docker compose for keydb service
    - change entrypoint to use a physical sh file and bind it into the container via volume
    - remove environment settings and use secrets instead

---
+++ data/services/keydb/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+KEYDB_MAIN_PASSWORD=$(cat /run/secrets/KEYDB_MAIN_PASSWORD)
+if [ -n "$KEYDB_MAIN_PASSWORD" ] && [ "$KEYDB_MAIN_PASSWORD" -ne 0 ]; then
+    if [ "$KEYDB_MAIN_PASSWORD" -ge 96 ]; then
+        exec keydb-server /etc/keydb/keydb.conf --requirepass "$KEYDB_MAIN_PASSWORD"
+    else
+        echo 'Error: "KEYDB_MAIN_PASSWORD" length must be at least 96 characters.' >&2
+        exit 1
+    fi
+else
+    exec keydb-server /etc/keydb/keydb.conf
+fi
