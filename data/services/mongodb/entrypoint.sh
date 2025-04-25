917af2ed3dbd86a39a08db551d6cdc1d1cfbf239
kiki-kanri <a470666@gmail.com>
2025-04-25 18:37:05 +0800

    fix: update MongoDB-related files
    - fix incorrect `MONGODB_MAIN_DATA_x_EXPOSE_PORT` env configuration
    - update entrypoint script: run replica set initialization in background
    - modify MongoDB startup method to properly handle SIG signals

--- data/services/mongodb/entrypoint.sh
+++ data/services/mongodb/entrypoint.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-cd "$(dirname "$(readlink -f "$0")")"
-LD_PRELOAD=./libforce_enable_thp.so python3 /usr/local/bin/docker-entrypoint.py \
-    --replSet rs0 &
+set -e
 
-if [ $(hostname) = "mongodb-main-data-1" ]; then
-    until mongosh --eval 'print(\"Waiting for MongoDB connection...\")' --quiet; do
+cd "$(dirname "$(readlink -f "$0")")"
+
+echo "[startup] Container started at $(date)."
+echo "[startup] Hostname: $(hostname)."
+
+ensure_replica_set_initialized() {
+    until mongosh --eval 'print("Waiting for MongoDB connection...")' --quiet; do
         sleep 1
     done
 
-    echo 'Initializing replica set...'
+    echo '[init] Initializing replica set...'
     mongosh --eval "
-      try {
-        rs.status();
-      } catch (error) {
-        rs.initiate({
-          _id: 'rs0',
-          members: [
-            { _id: 0, host: '$MONGODB_MAIN_RS_HOST:$MONGODB_MAIN_DATA_1_EXPOSE_PORT' },
-            { _id: 1, host: '$MONGODB_MAIN_RS_HOST:$MONGODB_MAIN_DATA_2_EXPOSE_PORT' },
-            { _id: 2, host: '$MONGODB_MAIN_RS_HOST:$MONGODB_MAIN_DATA_3_EXPOSE_PORT' },
-          ]
-        });
-      }
-    " --port 27017
+        try {
+            rs.status();
+        } catch (error) {
+            rs.initiate({
+                _id: 'rs0',
+                members: [
+                    { _id: 0, host: '$MONGODB_MAIN_RS_HOST:$MONGODB_MAIN_DATA_1_EXPOSE_PORT' },
+                    { _id: 1, host: '$MONGODB_MAIN_RS_HOST:$MONGODB_MAIN_DATA_2_EXPOSE_PORT' },
+                    { _id: 2, host: '$MONGODB_MAIN_RS_HOST:$MONGODB_MAIN_DATA_3_EXPOSE_PORT' },
+                ]
+            });
+        }
+    "
 
-    echo 'Replica set initialized.'
-fi
+    echo '[init] Replica set initialized.'
+}
 
-tail -f /dev/null
+handle_sigterm() {
+    echo '[trap] SIGTERM caught, forwarding to mongod...'
+    pkill -TERM mongod
+    echo '[trap] Waiting for mongod to exit...'
+    wait $PY_PID
+}
+
+[ "$(hostname)" = 'mongodb-main-data-1' ] && ensure_replica_set_initialized &
+LD_PRELOAD='./libforce_enable_thp.so' python3 /usr/local/bin/docker-entrypoint.py --replSet rs0 &
+PY_PID=$!
+trap handle_sigterm SIGTERM SIGINT
+wait $PY_PID
