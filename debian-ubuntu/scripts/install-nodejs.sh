2cf932d64c54dd7d900241bf3f01dc7fb70b2266
kiki-kanri <83924639+kiki-kanri@users.noreply.github.com>
2025-05-05 15:42:36 +0800

    fix: fixed error when exec install-nodejs script

--- debian-ubuntu/scripts/install-nodejs.sh
+++ debian-ubuntu/scripts/install-nodejs.sh
@@ -10,4 +10,4 @@ sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs
-../etc/cron.daily/upgrade-npm-packages
+bash ../etc/cron.daily/upgrade-npm-packages
