ccdafe1d31e7a440c6789fa2be80a2f39d9794a2
kiki-kanri <a470666@gmail.com>
2025-05-08 13:24:15 +0800

    chore: upgrade node version to 24

--- debian-ubuntu/scripts/install-nodejs.sh
+++ debian-ubuntu/scripts/install-nodejs.sh
@@ -7,7 +7,7 @@ cd "$ROOT_DIR"
 . ./scripts/common.sh
 
 sudo apt-get update
-curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs
 ./etc/cron.daily/upgrade-npm-packages
