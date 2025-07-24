e4608bcfb7e4838d5f7c518df26c49c1df50cc90
kiki-kanri <a470666@gmail.com>
2025-07-24 11:00:07 +0800

    chore: bump 7zip version

--- debian-ubuntu/scripts/install-7zip.sh
+++ debian-ubuntu/scripts/install-7zip.sh
@@ -10,8 +10,9 @@ cd "${BASE_DIR}"
 sudo rm -rf /opt/7zip
 sudo mkdir -p /opt/7zip
 cd /opt/7zip
-sudo wget https://www.7-zip.org/a/7z2409-linux-x64.tar.xz
-sudo tar -xvf 7z2409-linux-x64.tar.xz
-sudo rm 7z2409-linux-x64.tar.xz
+sudo wget https://www.7-zip.org/a/7z2500-linux-x64.tar.xz
+sudo tar -xvf 7z2500-linux-x64.tar.xz
+sudo rm 7z2500-linux-x64.tar.xz
+sudo rm -rf /usr/local/bin/7zz /usr/local/bin/7zzs
 sudo ln -s /opt/7zip/7zz /usr/local/bin/7zz
 sudo ln -s /opt/7zip/7zzs /usr/local/bin/7zzs
