d2a98e9e8afd35420508d32797e34bce4470068e
kiki-kanri <a470666@gmail.com>
2025-07-24 11:00:43 +0800

    chore: update install docker script

--- debian-ubuntu/scripts/install-docker.sh
+++ debian-ubuntu/scripts/install-docker.sh
@@ -14,13 +14,23 @@ elif [ "${os_type}" = 'ubuntu' ]; then
 fi
 
 sudo apt-get update
-sudo apt-get remove --auto-remove --purge ${to_remove_packages}
+sudo apt-get remove --auto-remove --purge "${to_remove_packages}"
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL "https://download.docker.com/linux/${os_type}/gpg" -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/${os_type}" $(. /etc/os-release && echo "${VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/${os_type}" \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
 sudo apt-get update
-sudo apt-get install -y containerd.io docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin
+sudo apt-get install \
+    containerd.io \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    docker-buildx-plugin
+
 sudo systemctl enable docker
 sudo systemctl start docker
