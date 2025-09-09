ca5db3acb665ce9558445a49a4345e7324170bc8
kiki-kanri <a470666@gmail.com>
2025-09-09 09:29:57 +0800

    chore: upgrade 7z version

--- debian-ubuntu/scripts/install-7zip.sh
+++ debian-ubuntu/scripts/install-7zip.sh
@@ -7,12 +7,16 @@ cd "${BASE_DIR}"
 
 . ./scripts/common.sh
 
+# Constants
+FILE_NAME="7z2501-linux-x64.tar.xz"
+
+# Run
 sudo rm -rf /opt/7zip
 sudo mkdir -p /opt/7zip
 cd /opt/7zip
-sudo wget https://www.7-zip.org/a/7z2500-linux-x64.tar.xz
-sudo tar -xvf 7z2500-linux-x64.tar.xz
-sudo rm 7z2500-linux-x64.tar.xz
+sudo wget "https://www.7-zip.org/a/${FILE_NAME}"
+sudo tar -xvf "${FILE_NAME}"
+sudo rm "${FILE_NAME}"
 sudo rm -rf /usr/local/bin/7zz /usr/local/bin/7zzs
 sudo ln -s /opt/7zip/7zz /usr/local/bin/7zz
 sudo ln -s /opt/7zip/7zzs /usr/local/bin/7zzs
