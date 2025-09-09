3771b623abf5133d1352069f670b1b63389db78f
kiki-kanri <a470666@gmail.com>
2025-09-09 09:28:40 +0800

    chore: install more php packages

--- debian-ubuntu/scripts/install-php8.sh
+++ debian-ubuntu/scripts/install-php8.sh
@@ -10,6 +10,6 @@ cd "${BASE_DIR}"
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates lsb-release software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-sudo apt-get install php8.4 php8.4-{bcmath,bz2,cli,common,curl,fpm,gd,gmp,http,imagick,imap,mbstring,mongodb,mysql,readline,redis,swoole,xml,yaml,zip}
+sudo apt-get install php8.4 php8.4-{bcmath,bz2,cli,common,curl,fpm,gd,gmp,http,imagick,imap,mbstring,mongodb,mysql,pcov,pq,raphf,readline,redis,swoole,xml,yaml,zip,zstd}
 sudo systemctl enable php8.4-fpm
 sudo systemctl start php8.4-fpm
