f589e9739baaa65969c29eaba50c4f74e892433d
kiki-kanri <a470666@gmail.com>
2025-10-08 13:19:56 +0800

    chore: format scripts

--- debian-ubuntu/scripts/install-docker.sh
+++ debian-ubuntu/scripts/install-docker.sh
@@ -12,11 +12,11 @@ sudo apt-get update
 
 if [ "${os_type}" = 'debian' ]; then
     for package in docker.io docker-doc docker-compose podman-docker containerd runc; do
-        sudo apt-get remove -y --auto-remove --purge "${package}";
+        sudo apt-get remove -y --auto-remove --purge "${package}"
     done
 elif [ "${os_type}" = 'ubuntu' ]; then
     for package in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-        sudo apt-get remove -y --auto-remove --purge "${package}";
+        sudo apt-get remove -y --auto-remove --purge "${package}"
     done
 fi
 
@@ -25,9 +25,9 @@ sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL "https://download.docker.com/linux/${os_type}/gpg" -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/${os_type}" \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/${os_type}" \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt-get update
 sudo apt-get install \
