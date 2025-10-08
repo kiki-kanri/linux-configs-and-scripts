2ac02f264e46c952c3127df8d7bc5583b0a02291
kiki-kanri <a470666@gmail.com>
2025-10-08 13:21:17 +0800

    chore: update install docker script

--- debian-ubuntu/scripts/install-docker.sh
+++ debian-ubuntu/scripts/install-docker.sh
@@ -26,7 +26,7 @@ sudo curl -fsSL "https://download.docker.com/linux/${os_type}/gpg" -o /etc/apt/k
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/${os_type}" \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+	$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt-get update
