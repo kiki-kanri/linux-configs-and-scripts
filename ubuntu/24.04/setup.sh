b9fcc1bbeb17c171266e3b5f52a58cb9c6e59644
kiki-kanri <a470666@gmail.com>
2025-12-08 16:58:34 +0800

    feat: update ubuntu setup script

--- ubuntu/24.04/setup.sh
+++ ubuntu/24.04/setup.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
 # Functions
-install_file() {
-    local mode="${1}"
-    local src="${2}"
-    local user="${3:-root}"
-    local group="${4:-$user}"
-
-    local src_path="${BASE_DIR}/${src}"
-    local dest="/${src}"
-
-    echo "Installing ${src_path} -> ${dest} (mode=${mode}, owner=${user}:${group})"
-    if [[ ! -f "${src_path}" ]]; then
-        log_red "Source file not found: ${src_path}"
-        return 1
-    fi
-
-    sudo install -o "${user}" -g "${group}" -Dm"${mode}" "${src_path}" "${dest}"
-}
-
 log_green() {
     echo -e "\033[32m$*\033[0m"
 }
@@ -36,6 +18,32 @@ git clone https://github.com/kiki-kanri/linux-configs-and-scripts
 cd ./linux-configs-and-scripts/ubuntu/24.04/
 
 log_green 'Installing base packages...'
-apt-get install -y bash-completion bsdmainutils htop locales lsd lsof vim ufw
+apt-get install -y bash-completion bsdmainutils htop locales lsd lsof rsync vim ufw
 
 log_green 'Installing files...'
+
+# Ask for SSH port
+while true; do
+    read -p "Please enter SSH port: " SSH_PORT
+    [[ "${SSH_PORT}" =~ ^[0-9]+$ ]] || {
+        log_red "Invalid port: must be a number"
+        continue
+    }
+
+    if (($SSH_PORT < 1 || $SSH_PORT > 65535)); then
+        log_red "Invalid port: must be 1-65535"
+        continue
+    fi
+
+    if ss -tulpn | grep -q ":${SSH_PORT}\b"; then
+        log_red "Port ${SSH_PORT} is already in use"
+        continue
+    fi
+
+    break
+done
+
+# Install files
+rsync -av --progress ./etc/ /etc/
+rsync -av --progress ./root/ /root/
+sed -i "s/SSH_PORT/${SSH_PORT}/" /etc/ssh/sshd_config
