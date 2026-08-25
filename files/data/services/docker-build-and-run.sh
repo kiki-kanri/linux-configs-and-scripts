6100853cb6eb65ef4750cffe47857f190649ee8c
kiki-kanri <a470666@gmail.com>
2026-08-25 13:41:28 +0800

    chore: update `docker-build-and-run.sh`

--- files/data/services/docker-build-and-run.sh
+++ files/data/services/docker-build-and-run.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
 find ./stacks -type d -name opt -exec chmod 755 {} +
-find ./stacks -type f -path '*/opt/*.sh' -exec chmod 755 {} +
+find ./stacks -type f -path '*.sh' -exec chmod 755 {} +
 
 for arg in "$@"; do
     if [[ "${arg}" == '-p' ]]; then
