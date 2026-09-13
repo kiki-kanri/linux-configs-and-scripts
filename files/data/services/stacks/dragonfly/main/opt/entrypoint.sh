4d42fc10d80f55cc75b1c386376bb7f6a4567196
kiki-kanri <a470666@gmail.com>
2026-09-14 00:02:05 +0800

    fix(dragonfly): enable BullMQ hashtag locking

--- files/data/services/stacks/dragonfly/main/opt/entrypoint.sh
+++ files/data/services/stacks/dragonfly/main/opt/entrypoint.sh
@@ -14,11 +14,11 @@ fi
 
 if [ -n "${DRAGONFLY_MAIN_PASSWORD}" ]; then
     if [ "${#DRAGONFLY_MAIN_PASSWORD}" -ge 96 ]; then
-        exec dragonfly --logtostderr --requirepass "${DRAGONFLY_MAIN_PASSWORD}"
+        exec dragonfly --logtostderr --requirepass "${DRAGONFLY_MAIN_PASSWORD}" "$@"
     else
         echo 'Error: "DRAGONFLY_MAIN_PASSWORD" length must be at least 96 characters' >&2
         exit 1
     fi
 else
-    exec dragonfly --logtostderr
+    exec dragonfly --logtostderr "$@"
 fi
